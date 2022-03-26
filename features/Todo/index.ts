7db8f984d1d20eda3e49ec9c2d7aedddcc9af08b
Tornike Shavidze <shavidzet@gmail.com>
2022-03-26 20:12:29 +0400

    Persist todo list(local data) in localStorage (#5)
    * persist todo list data in localStorage
    
    * Notify user when persistent onload crashes
    
    * fix persistKey read and generate
    
    * refactor persisted list parsing from localStorage
    
    * add autofocus for todo add input

--- features/Todo/index.ts
+++ features/Todo/index.ts
@@ -28,9 +28,30 @@ function todoResponse<T> (success: boolean, data: ITodoResponse<T>['data']) {
   }
 }
 
+interface ITodoCallbacks {
+  onPersistentDataInitError? (): void,
+}
+
 class TodoClient implements ITodoClient {
   private list: TodoList = []
+  private persistKey = 'todoList'
   emptyTodoItem: TodoItem = { id: '', name: '' }
+
+  constructor (callbacks: ITodoCallbacks = {}) {
+    const persistedListUnnormalized = localStorage.getItem(this.persistKey)
+    if (!persistedListUnnormalized) {
+      return
+    }
+    const persistedList = this.parsePersistentList(persistedListUnnormalized)
+    if (persistedList && this.isListValid(persistedList)) {
+      this.list = persistedList
+      return
+    }
+    if (callbacks.onPersistentDataInitError) {
+      callbacks.onPersistentDataInitError()
+    }
+    localStorage.setItem(this.persistKey, JSON.stringify([]))
+  }
 
   create (name: TodoItem['name']) {
     const newItem = {
@@ -38,6 +59,7 @@ class TodoClient implements ITodoClient {
       name
     }
     this.list.push(newItem)
+    this.updatePersistentStorage(this.list)
     return todoResponse(true, newItem)
   }
 
@@ -59,6 +81,7 @@ class TodoClient implements ITodoClient {
 
     if (targetItem) {
       targetItem.name = name
+      this.updatePersistentStorage(this.list)
       return todoResponse(true, targetItem)
     }
 
@@ -69,10 +92,27 @@ class TodoClient implements ITodoClient {
     const targetIndex = this.list.findIndex(item => item.id === id)
     if (targetIndex > -1) {
       const deletedItem = this.list.splice(targetIndex, 1)[0]
+      this.updatePersistentStorage(this.list)
       return todoResponse(true, deletedItem)
     }
 
     return todoResponse(false, this.emptyTodoItem)
+  }
+
+  private updatePersistentStorage (list: TodoList) {
+    localStorage.setItem(this.persistKey, JSON.stringify(list))
+  }
+
+  private isListValid (list: TodoList) {
+    return Array.isArray(list)
+  }
+
+  private parsePersistentList (list: string) {
+    try {
+      return JSON.parse(list)
+    } catch (error) {
+      return false
+    }
   }
 }
 
